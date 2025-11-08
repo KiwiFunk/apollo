3995bb9990f3fa6b506ed971681d9160e99439a0
Alex <alex_p97@live.com>
2025-11-08 16:03:31 +0000

    Added Preact for dynamic functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,19 @@ import { defineConfig } from 'astro/config';
 import node from "@astrojs/node";
 import tailwindcss from '@tailwindcss/vite';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+
   adapter: node({
     mode: "standalone"
   }),
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [preact()]
 });
