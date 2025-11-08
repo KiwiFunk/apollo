615f537c2b1adbb065a81617a568b89942e15c83
Alex <alex_p97@live.com>
2025-11-08 21:31:20 +0000

    Added Clear button to search bar

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -71,6 +71,20 @@ export default function Search({ searchList }: Props) {
         placeholder="Search notes..."
         class="block w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md leading-5 bg-white placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
       />
+
+      {/* Clear Button */}
+      {query && (
+        <button
+          type="button"
+          class="absolute inset-y-0 right-0 pr-3 flex items-center text-gray-400 hover:text-gray-600 cursor-pointer"
+          onClick={() => setQuery('')}
+          aria-label="Clear search"
+        >
+          <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+            <path fill-rule="evenodd" d="M10 8.586l4.95-4.95a1 1 0 111.414 1.414L11.414 10l4.95 4.95a1 1 0 01-1.414 1.414L10 11.414l-4.95 4.95a1 1 0 01-1.414-1.414L8.586 10l-4.95-4.95a1 1 0 011.414-1.414L10 8.586z" clip-rule="evenodd" />
+          </svg>
+        </button>
+      )}
       
       {/* Search Results Dropdown */}
       {query.length > 1 && (
