385d19bd063c9be93f930956609fcafb04ca6064
Alex <alex_p97@live.com>
2025-11-10 18:38:43 +0000

    Corrected param publish_date -> publishDate

--- src/components/StatsBar.tsx
+++ src/components/StatsBar.tsx
@@ -10,7 +10,7 @@ import type { Frontmatter } from "../types";
 export default function StatsBar({ frontmatter, htmlContent }: { frontmatter: Frontmatter, htmlContent: string }) {
 
     // Deconstruct frontmatter to get title and publish_date
-    const { title, publish_date } = frontmatter;
+    const { title, publishDate } = frontmatter;
 
     const calcReadingTime = () => {
         // Simple reading time calculation: average adult reads about 200-250 words per minute
@@ -36,7 +36,7 @@ export default function StatsBar({ frontmatter, htmlContent }: { frontmatter: Fr
                 <div>
                     <span class="flex-col">
                         <h2 class="text-xs font-semibold text-gray-600 uppercase tracking-wider">Published:</h2>
-                        {publish_date}
+                        {publishDate}
                     </span>
 
                     <span class="flex-col">
