80adee43e55d6de80eb6ee97ecd221472c23ad2f
Alex <alex_p97@live.com>
2025-11-10 20:56:31 +0000

    Created state/toggle func for statsbar open/close

--- src/components/StatsBar.tsx
+++ src/components/StatsBar.tsx
@@ -1,13 +1,10 @@
-// Here we pull the metadata for the note and display it in a stats bar at the top of the note view.
-
-// { title } will be doucment name
-
-// { publish_date } we need to extract date only since we dont dother collecting timestamp
-
-//
+import { useState } from 'preact/hooks';
 import type { Frontmatter } from "../types";
 
 export default function StatsBar({ frontmatter, htmlContent, handleBackClick }: { frontmatter: Frontmatter, htmlContent: string, handleBackClick: () => void }) {
+
+    // State to manage the visibility of the stats section
+    const [isOpen, setIsOpen] = useState(true);
 
     // Deconstruct frontmatter to get title, category and publish_date
     const { title, publishDate, category } = frontmatter;
@@ -31,10 +28,14 @@ export default function StatsBar({ frontmatter, htmlContent, handleBackClick }: 
         else return `${Math.ceil(result)} min read`;
     }
 
+    const handleToggle = () => {
+        setIsOpen(!isOpen);
+    }
+
     return (
         <div class="bg-white border-b border-gray-200 w-full shadow-sm">
             {/* Upper Section */}
-            <div class="flex items-center justify-between px-7 pt-5">
+            <div class="flex items-center justify-between px-7 pt-4">
 
                 <div>
                     <h4 class="text-gray-400 text-xs font-semibold uppercase tracking-widest">Note Title</h4>
@@ -60,15 +61,25 @@ export default function StatsBar({ frontmatter, htmlContent, handleBackClick }: 
                 </div>
             </div>
 
-            <div class="border-t border-gray-200 my-5" />
+            <div class="border-t border-gray-200 my-4" />
     
             {/* Lower Section */}
-            <div class="flex flex-row gap-5 items-baseline px-7">
-                <button onClick={handleBackClick} className="text-indigo-600 text-xs font-semibold uppercase tracking-wider hover:underline cursor-pointer">
-                    &larr; Home
-                </button>
+            <div>
 
-                <p class="text-xs font-semibold text-gray-500 uppercase tracking-wider m-0 mb-5">{ category }</p>
+                {/* Breadcrumbs */}
+                <div class="flex flex-row gap-5 items-baseline px-7">
+                    <button onClick={handleBackClick} className="text-indigo-600 text-xs font-semibold uppercase tracking-wider hover:underline cursor-pointer">
+                        &larr; Home
+                    </button>
+
+                    <p class="text-xs font-semibold text-gray-500 uppercase tracking-wider m-0 mb-4">{ category }</p>
+                </div>
+
+                {/* Show/Hide Toggle */}
+                <div>
+
+                </div>
+
             </div>
         </div>
 
