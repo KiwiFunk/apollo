5a2c04f9edead3fcffd96ab46b97bf49b365c629
Alex <alex_p97@live.com>
2025-11-10 18:27:20 +0000

    Created basic statsBar component to display frontmatter for each note

---
+++ src/components/StatsBar.tsx
@@ -0,0 +1,58 @@
+// Here we pull the metadata for the note and display it in a stats bar at the top of the note view.
+
+// { title } will be doucment name
+
+// { publish_date } we need to extract date only since we dont dother collecting timestamp
+
+//
+import type { Frontmatter } from "../types";
+
+export default function StatsBar({ frontmatter, htmlContent }: { frontmatter: Frontmatter, htmlContent: string }) {
+
+    // Deconstruct frontmatter to get title and publish_date
+    const { title, publish_date } = frontmatter;
+
+    const calcReadingTime = () => {
+        // Simple reading time calculation: average adult reads about 200-250 words per minute
+        const wordsPerMinute = 225;
+        const totalWords = htmlContent.split(/\s+/).length;
+        
+        const result = totalWords / wordsPerMinute;
+        
+        if (result < 1) return "Less than a minute";
+        else return `${Math.ceil(result)} min read`;
+    }
+
+    return (
+        <div class="fixed">
+            {/* Upper Section */}
+            <div class="flex items-center justify-between">
+                <div>
+                    <h2 class="text-xs font-semibold text-gray-600 uppercase tracking-wider">Note Title</h2>
+                    <h1>{title}</h1>
+                </div>
+
+                {/* Note Statistics */}
+                <div>
+                    <span class="flex-col">
+                        <h2 class="text-xs font-semibold text-gray-600 uppercase tracking-wider">Published:</h2>
+                        {publish_date}
+                    </span>
+
+                    <span class="flex-col">
+                        <h2 class="text-xs font-semibold text-gray-600 uppercase tracking-wider">Reading Time:</h2>
+                        {calcReadingTime()}
+                    </span>
+                </div>
+            </div>
+
+            <hr className="my-4" />
+
+            {/* Lower Section */}
+            <div>
+
+            </div>
+        </div>
+
+    )
+}
