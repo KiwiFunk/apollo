526cc6f9fd29ef1c2f641a165c73fcef016099ba
Alex <alex_p97@live.com>
2025-11-11 22:10:14 +0000

    Set up basic auth method in BetterAuth config

--- auth.ts
+++ auth.ts
@@ -3,7 +3,25 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "./src/db"; // your drizzle instance
 
 export const auth = betterAuth({
+
+    // Connect Better Auth to Drizzle ORM for DB connection
     database: drizzleAdapter(db, {
         provider: "pg", // or "mysql", "sqlite"
     }),
+
+    // Basic Email/Password
+    emailAndPassword: {
+        enabled: true,
+    },
+
+    // oAuth Providers (Set Up Later)
+
+    /*
+    socialProviders: { 
+        github: { 
+        clientId: process.env.GITHUB_CLIENT_ID as string, 
+        clientSecret: process.env.GITHUB_CLIENT_SECRET as string, 
+        }, 
+    }, 
+    */
 });
