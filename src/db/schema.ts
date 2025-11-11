7996860e6ab95f859fdb7abd1fbe051b1800405b
Alex <alex_p97@live.com>
2025-11-11 21:55:52 +0000

    Removed old tables, started adding Better-Auth tables

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,34 +1,65 @@
 import { pgTable, serial, text, timestamp, varchar } from 'drizzle-orm/pg-core';
 
-// Users Table
-export const users = pgTable('users', {
-  id: varchar('id', { length: 255 }).primaryKey(),
-  username: text('username').unique(),
-  hashedPassword: text('hashed_password'),
-});
-
-// Sessions Table (Required by Lucia)
-export const sessions = pgTable('sessions', {
-  id: text('id').primaryKey(),
-  userId: varchar('user_id', { length: 255 })
-    .notNull()
-    .references(() => users.id),
-  expiresAt: timestamp('expires_at', {
-    withTimezone: true,
-    mode: 'date',
-  }).notNull(),
-});
-
-// Notes Table (updated with a userId)
+// Notes Table
 export const notes = pgTable('notes', {
   id: serial('id').primaryKey(),
-  userId: varchar('user_id', { length: 255 })
+  userId: text('user_id')
     .notNull()
-    .references(() => users.id),
+    .references(() => user.id, { onDelete: 'cascade' }),
   slug: text('slug').unique().notNull(),
   title: text('title').notNull(),
   description: text('description'),
   publishDate: timestamp('publish_date', { mode: 'date' }),
   category: text('category'),
   content: text('content'),
+});
+
+// Better-Auth tables
+export const user = pgTable('user', {
+    id: text('id').primaryKey(),
+    name: text('name'),
+    email: text('email').notNull().unique(),
+    emailVerified: boolean('email_verified').default(false),
+    image: text('image'), // Nullable by default
+    createdAt: timestamp('created_at', { withTimezone: true }).defaultNow().notNull(),
+    updatedAt: timestamp('updated_at', { withTimezone: true }).defaultNow().notNull(),
+});
+
+export const session = pgTable('session', {
+    id: text('id').primaryKey(),
+    // FK between a session and a user.
+    userId: text('user_id')
+        .notNull()
+        .references(() => user.id, { onDelete: 'cascade' }),
+    token: text('token').notNull().unique(),
+    expiresAt: timestamp('expires_at', { withTimezone: true }).notNull(),
+    ipAddress: text('ip_address'),
+    userAgent: text('user_agent'),
+    createdAt: timestamp('created_at', { withTimezone: true }).defaultNow().notNull(),
+    updatedAt: timestamp('updated_at', { withTimezone: true }).defaultNow().notNull(),
+});
+
+export const account = pgTable('account', {
+    id: text('id').primaryKey(),
+    // FK between an account and a user.
+    userId: text('user_id')
+        .notNull()
+        .references(() => user.id, { onDelete: 'cascade' }),
+    accountId: text('account_id').notNull(),
+    providerId: text('provider_id').notNull(),
+    // oAuth token specific
+    accessToken: text('access_token'),
+    refreshToken: text('refresh_token'),
+    accessTokenExpiresAt: timestamp('access_token_expires_at', { withTimezone: true }),
+    refreshTokenExpiresAt: timestamp('refresh_token_expires_at', { withTimezone: true }),
+    scope: text('scope'),
+    idToken: text('id_token'),
+    // Store HASHED password
+    password: text('password'),
+    createdAt: timestamp('created_at', { withTimezone: true }).defaultNow().notNull(),
+    updatedAt: timestamp('updated_at', { withTimezone: true }).defaultNow().notNull(),
+});
+
+export const verification = pgTable('verification', {
+
 });
