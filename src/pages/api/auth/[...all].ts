cc88723552f8f6712c8d7f80fe4cbefc5138e687
Alex <alex_p97@live.com>
2025-11-11 22:18:29 +0000

    Created endpoint to mount BetterAuth handler

---
+++ src/pages/api/auth/[...all].ts
@@ -0,0 +1,12 @@
+import { auth } from "../../../../auth"; // Path to auth.ts
+import type { APIRoute } from "astro";
+
+// Important! Tell Astro endpoint is dynamic and must be run on the server for every request.
+export const prerender = false;
+
+// The 'ALL' export handles any HTTP method (GET, POST, PUT, etc.)
+export const ALL: APIRoute = async ({ request }) => {
+  // Handoff request to the Better Auth handler.
+  // Automatically handles all auth-related API endpoints like /login, /logout, /callback, etc.
+  return auth.handler(request);
+};
