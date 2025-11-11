09d3290dcc6d6a0003c769fd255372f9b41eec2b
Alex <alex_p97@live.com>
2025-11-11 19:02:49 +0000

    Created Drizzle config

---
+++ drizzle.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from 'drizzle-kit';
+import 'dotenv/config';
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL is not set in .env file');
+}
+
+export default {
+  schema: './src/db/schema.ts',
+  out: './src/db/migrations',
+  driver: 'pg',
+  dbCredentials: {
+    connectionString: process.env.DATABASE_URL,
+  },
+} satisfies Config;
