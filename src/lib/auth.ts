ecfa3eb373f24c6881200b2362b2679b2cb4ef60
Alex <alex_p97@live.com>
2025-11-12 18:17:20 +0000

    Created middleware for validating session cookie, and making session data avaliable through locals

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,6 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { db } from "./src/db"; // your drizzle instance
+import { db } from "../db";
 
 export const auth = betterAuth({
 
---
+++ src/middleware.ts
@@ -0,0 +1,19 @@
+import { auth } from "./lib/auth";
+import { defineMiddleware } from "astro:middleware";
+
+export const onRequest = defineMiddleware(async (context, next) => {
+    
+    // Hand the request to Better Auth to get the session.
+    const session = await auth.api.getSession({
+        headers: context.request.headers,
+    });
+
+    // The session object contains the user, so we can derive the user from it.
+    const user = session?.user ?? null;
+
+    // Attach the session and user data to `context.locals`.
+    context.locals = { session, user: session?.user ?? null };
+
+    // Continue to the requested page.
+    return next();
+});
